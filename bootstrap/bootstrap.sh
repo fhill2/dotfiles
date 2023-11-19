ef5a6c6f5c7f9e943550252f4f72b68b3a93ac36
Freddie Hill <freddiehill000@gmail.com>
2023-11-19 19:45:40 +0000

    19/11/2023

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -453,6 +453,7 @@ function setup_zsh_symlinks() {
   ln -s ~/dot/config/zsh ~/.zsh
   ln -s ~/dot/config/zsh/zshenv ~/.zshenv
   ln -s ~/dot/config/zsh/zprofile ~/.zprofile
+  ln -s ~/dot/config/profile ~/.profile
   if [[ "$(uname -s)" == "Darwin" ]]; then
     ln -s ~/dot/config/profile_osx ~/.profile
   else
