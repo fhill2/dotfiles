33a1854459552047a9b4e73cf3a68c6e27fd17ec
Freddie Hill <freddiehill000@gmail.com>
2023-12-03 21:49:06 +0000

    3/12/2023

--- bootstrap/defaults.sh
+++ bootstrap/defaults.sh
@@ -2,6 +2,11 @@
 # my osx defaults
 
 
+# ____
+# requirements 2023:
+brew install duti
+# ____
+
 # JUN 2023 Still to automate:
 # Sound > Alert Volume to 0. This is because of key repeat + karabiner. key repeat triggers beep on tab_as_hyper.
 
