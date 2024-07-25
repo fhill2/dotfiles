b2c2fc113b0c64ca6899a12a195f2445b85d565f
Freddie Hill <freddiehill000@gmail.com>
2024-07-25 19:34:16 +0100

    25/07/2024

---
+++ bootstrap/1_shell_and_symlinks.sh
@@ -0,0 +1,30 @@
+function chsh_zsh() {
+	if [[ "$SHELL" != "$(zsh_binary)" ]]; then
+		echo "Changing shell for $current_user..."
+
+		local current_user=$USER
+		sudo chsh -s $(zsh_binary) $current_user
+	fi
+}
+function setup_zsh_symlinks() {
+	ln -s ~/dot/config/zsh/zshrc ~/.zshrc
+	ln -s ~/dot/config/zsh ~/.zsh
+	ln -s ~/dot/config/zsh/zshenv ~/.zshenv
+	ln -s ~/dot/config/zsh/zprofile ~/.zprofile
+	if [[ "$(uname -s)" == "Darwin" ]]; then
+		ln -s ~/dot/config/profile_osx ~/.profile
+	else
+		ln -s ~/dot/config/profile_linux ~/.profile
+	fi
+	source ~/.zshrc
+}
+
+mkdir -p $HOME/.config
+setup_zsh_symlinks
+
+echo "Replacing local bin with dotfiles bin..."
+ln -s ~/dot/bin ~/.local/bin
+
+# Generate SSH key as osx package list is in a shared private repo
+# -C -> comment
+ssh-keygen -t rsa -b 4096 -C "freddiehill000@gmail.com"
---
+++ bootstrap/3_install_homebrew.sh
@@ -0,0 +1,15 @@
+# Run before the main Brewfile install
+	if test ! $(which brew); then
+		echo "Installing Homebrew..."
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		(
+			echo
+			echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+		) >>/Users/s1/.zprofile
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+		brew update
+		brew install git
+	fi
+}
+
+
