460adbf53c67b49cefba3f5efe3d825356a3a56a
Freddie Hill <freddiehill000@gmail.com>
2025-05-17 18:38:26 +0100

    ssh osc52 documentation

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -11,13 +11,15 @@ vim.o.swapfile = false
 vim.g.omni_sql_no_default_maps = 1
 vim.g.ftplugin_sql_omni_key = ""
 
--- 2 Configuration Options are needed to copy/paste text from/to neovim running in an SSH terminal
--- kitty.conf -> clipboard_control setting enabled
--- osc52 clipboard when neovim is opened in an SSH terminal
--- vim.o.clipboard = "unnamedplus" just syncs the default neovim register with the system clipboard register
+-- 3 Configuration Options are needed to copy/paste text from/to neovim running in an SSH terminal
+-- 1) kitty.conf -> clipboard_control setting enabled
+-- 2) osc52 clipboard when neovim is opened in an SSH terminal
+-- 2.5) vim.o.clipboard = "unnamedplus" just syncs the default neovim register with the system clipboard register
 -- this means "+y becomes y
+-- ... Using the above will work without tmux. If the remote session is nested in tmux, additional configuration is required:
+-- 3)
+-- see nvim-osc52-clipboard-ssh-registers obsidian for more info.
 if vim.env.SSH_TTY then
   vim.g.clipboard = "osc52"
 end
-
 vim.o.clipboard = "unnamedplus"
